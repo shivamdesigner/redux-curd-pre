ef90100b44539ef064b2e5b970ed63e27aed4856
shivam-impero <shivam.pavar@imperoit.com>
2025-03-07 18:18:22 +0530

    changes

--- new/js.js
+++ new/js.js
@@ -1,16 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector(".navbar-sticky");
-    const navbarToggle = document.querySelector(".navbar-toggler");
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-      } else {
-        navbar.classList.remove("scrolled");
-      }
+// document.addEventListener("DOMContentLoaded", function () {
+//     const navbar = document.querySelector(".navbar-sticky");
+//     const navbarToggle = document.querySelector(".navbar-toggler");
+//     window.addEventListener("scroll", function () {
+//       if (window.scrollY > 50) {
+//         navbar.classList.add("scrolled");
+//       } else {
+//         navbar.classList.remove("scrolled");
+//       }
+//     });
+//     navbarToggle.addEventListener("click", function () {
+//       if (window.innerWidth < 992) {
+//         navbar.classList.toggle("bg-dark", !navbar.classList.contains("bg-dark"));
+//       }
+//     });
+//   });
+
+  (function () {
+    const doc = document.documentElement;
+    const header = document.getElementById('site-header');
+    let prevScroll = window.scrollY || doc.scrollTop;
+    let prevDirection = 0;
+    const threshold = 200;
+
+    window.addEventListener('scroll', () => {
+        const curScroll = window.scrollY || doc.scrollTop;
+        const curDirection = curScroll > prevScroll ? 2 : 1;
+
+        if (curDirection !== prevDirection) {
+            if (curDirection === 2 && curScroll > threshold) {
+                header.classList.add('hide');
+                prevDirection = curDirection;
+            } else if (curDirection === 1) {
+                header.classList.remove('hide');
+                prevDirection = curDirection;
+            }
+        }
+
+        header.classList.toggle('scrolled', curScroll > threshold);
+        prevScroll = curScroll;
     });
-    navbarToggle.addEventListener("click", function () {
-      if (window.innerWidth < 992) {
-        navbar.classList.toggle("bg-dark", !navbar.classList.contains("bg-dark"));
-      }
-    });
-  });
+})();
